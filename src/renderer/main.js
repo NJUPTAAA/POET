b5bde8cd0b57f55db3ae475e71d7d0f05be71a4c
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-07-23 11:09:43 +0800

    init

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,11 @@ import axios from 'axios';
 import App from './App';
 import router from './router';
 import store from './store';
+import './lib/MDI-WXSS/MDI.css';
+import './lib/bootstrap-material-design/bootstrap-material-design.min.css';
+import './lib/animate/animate.min.css';
+import './lib/WeMD/wemd-color-scheme.css';
+import './assets/fonts/raleway.css';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
